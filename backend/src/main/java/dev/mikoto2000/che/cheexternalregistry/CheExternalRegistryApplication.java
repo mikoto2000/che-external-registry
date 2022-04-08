10b884548edde39cf1d448c96f9eb3826372dd3e
mikoto2000 <mikoto2000@gmail.com>
2022-04-08 23:43:09 +0900

    Added backend template code,from Spring initializr.

---
+++ backend/src/main/java/dev/mikoto2000/che/cheexternalregistry/CheExternalRegistryApplication.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.che.cheexternalregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheExternalRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheExternalRegistryApplication.class, args);
+	}
+
+}
