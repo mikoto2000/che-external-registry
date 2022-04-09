810afb807234e8d8f42db5e47c646628cb7a9d2f
mikoto2000 <mikoto2000@gmail.com>
2022-04-09 12:35:09 +0900

    WIP: 編集ボタンを押すと、テキストエリアに devfile が表示されるところまで。

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,9 +1,9 @@
-module Main exposing (Model, Msg(..), Stack, default_che_base_url, init, main, openshift_base_url, to_anchor, update, view)
+module Main exposing (Model, Msg(..), Stack, default_che_base_url, init, main, openshift_base_url, to_table_column, update, view)
 
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onClick, onInput)
 import Http
 import Json.Decode exposing (Decoder, field, list, map2, string)
 import List
@@ -49,12 +49,15 @@ type alias Stack =
 
 
 type alias Model =
-    { che_base_url : String, filter_text : String, stacks : List Stack }
+    { che_base_url : String
+    , filter_text : String
+    , devfile_content : String
+    , stacks : List Stack }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model default_che_base_url "" [], get_stacks )
+    ( Model default_che_base_url "" "" [], get_stacks )
 
 
 
@@ -65,6 +68,8 @@ type Msg
     = ChangeCheUrl String
     | ChangeFilterText String
     | GotStacks (Result Http.Error (List Stack))
+    | EditDevfile String
+    | GotDevfileContent (Result Http.Error String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -75,6 +80,17 @@ update msg model =
 
         ChangeFilterText new_filter_text ->
             ( { model | filter_text = new_filter_text }, Cmd.none )
+
+        EditDevfile devfile_url ->
+            ( model, get_devfile_content devfile_url )
+
+        GotDevfileContent result ->
+            case result of
+                Ok got_devfile_content ->
+                    ( { model | devfile_content = got_devfile_content }, Cmd.none )
+
+                Err _ ->
+                    ( { model | devfile_content = "devfile 取得に失敗しました。" }, Cmd.none )
 
         GotStacks result ->
             case result of
@@ -118,22 +134,33 @@ view model =
         , div []
             [ table []
                 [ tbody []
-                    (List.map2 to_anchor (List.repeat (List.length filtered_stacks) model.che_base_url) filtered_stacks)
+                    (List.map2 to_table_column (List.repeat (List.length filtered_stacks) model.che_base_url) filtered_stacks)
                 ]
             ]
+        , div []
+            [ textarea [ id "devfile" ]
+            [ text model.devfile_content ] ]
         ]
 
 
-to_anchor : String -> Stack -> Html Msg
-to_anchor che_base_url stack =
+to_table_column : String -> Stack -> Html Msg
+to_table_column che_base_url stack =
     tr []
         [ td [] [ a [ href (che_base_url ++ "/" ++ che_path ++ che_query_string_prefix ++ stack.url), title (che_base_url ++ " で開く") ] [ text stack.name ] ]
         , td [] [ a [ href stack.url, title "devfile" ] [ text "⚙" ] ]
+        , td [] [ a [ id "edit_button", title "devfile を編集", onClick (EditDevfile stack.url), style "cursor" "pointer"] [ text "📝" ] ]
         ]
 
 
 
 -- HTTP
+
+get_devfile_content : String -> Cmd Msg
+get_devfile_content url =
+    Http.get
+        { url = url
+        , expect = Http.expectString GotDevfileContent
+        }
 
 
 get_stacks : Cmd Msg
